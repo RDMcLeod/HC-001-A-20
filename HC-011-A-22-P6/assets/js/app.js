080a1d75ccd72bc50fafcfadf7331add2c4652f2
RDM <70675143+RDMcLeod@users.noreply.github.com>
2022-05-26 19:10:39 -0400

    adds header css/ js tgl nav menu

--- HC-011-A-22-P6/assets/js/app.js
+++ HC-011-A-22-P6/assets/js/app.js
@@ -1,4 +1,15 @@
 //!TOGGLE NAVIGATION MENU AND ICONS
+const menuToggleButton= document.querySelector('menu-toggle-button');
+const menuElement= document.querySelector('menu');
+
+const toggleMenu = () => {
+    menuElement.classList.toggle('active')
+    menuToggleButton.classList.toggle('active')
+}
+menuToggleButton.addEventListener('click', toggleMenu)
+
+
+
 
 // !REMOVE ACTIVE CLASS FROM AND ICON ON LINK CLICK
 

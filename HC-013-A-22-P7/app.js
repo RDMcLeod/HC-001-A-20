10ddf6ed9440e955042885fd9e0a9c32b0236bd1
RDM <70675143+RDMcLeod@users.noreply.github.com>
2022-08-11 22:11:03 -0400

    adds css header act/js headr

--- HC-013-A-22-P7/app.js
+++ HC-013-A-22-P7/app.js
@@ -1,4 +1,14 @@
 //! === ADD HEADER STYLES ON SCROLL ===
+const headerScroll = () => {
+    const headerElement = document.querySelector('.header');
+    this.scrollY >= 30 ? headerElement.classList.add('active'): headerElement.classList.remove('active');
+}
+window.addEventListener('scroll', headerScroll);
+
+
+
+
+
 
 //! === OPEN AND CLOSE THE MENU ON HAMBURGER ICON CLICK ===
 

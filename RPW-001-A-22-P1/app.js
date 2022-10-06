d7b6167775727b8329ba4b488b38919fd60f7a27
RDM <70675143+RDMcLeod@users.noreply.github.com>
2022-10-06 19:38:29 -0400

    update files from 013 file

--- RPW-001-A-22-P1/app.js
+++ RPW-001-A-22-P1/app.js
@@ -36,3 +36,13 @@ const swiper = new swiper('.myswiper',{
     }
 })
 //! === SCROLL REVEAL ===
+const sr = ScrollReveal({
+    distance: '50px',
+    duration: 1500,
+    easing:'cubic-bezier(0.68, -0.55, 0.265, 1.55)'
+
+});
+sr.reveal('.showcase_title, .showcase_image, .section_metadata, .swiper',{
+    origin: 'top',
+    interval:250
+});

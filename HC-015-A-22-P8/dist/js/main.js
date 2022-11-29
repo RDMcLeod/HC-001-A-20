becbb1c8366e10e0e6f0e9accd7e14658d2ad834
RDM <70675143+RDMcLeod@users.noreply.github.com>
2022-11-28 19:51:56 -0500

    adds / listeners/main.js

--- HC-015-A-22-P8/dist/js/main.js
+++ HC-015-A-22-P8/dist/js/main.js
@@ -1,6 +1,17 @@
 const initApp = () =>{
     //add listeners
-
+    const geoButton = document.getElementById("getLocation");
+    geoButton.addEventListener("click", getGeoWeather);
+    const homeButton = document.getElementById("home");
+    homeButton.addEventListener("click", loadWeather);
+    const saveButton = document.getElementById("saveLocation");
+    saveButton.addEventListener("click", saveLocation);
+    const unitButton = document.getElementById("unit");
+    unitButton.addEventListener("click", setUnitPref);
+    const refreshButton = document.getElementById("refresh");
+    refreshButton.addEventListener("click", refreshWeather);
+    const locationEntry = document.getElementById("searchBar__form");
+    locationEntry.addEventListener("submit", submitNewLocation);
 
     // set up
 

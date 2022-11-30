a9513cdb8a07944b00e4840aa884cf8e524da873
RDM <70675143+RDMcLeod@users.noreply.github.com>
2022-11-29 22:41:51 -0500

    adds/currentLocation.js

---
+++ HC-015-A-22-P8/dist/js/CurrentLocation.js
@@ -0,0 +1,26 @@
+export default class CurrentLocation{
+    constructor () {
+        this._name = "Current Location";
+        this._lat = null;
+        this._lon = null;
+        this._unit = "imperial";
+    }
+    getName() {
+        return this._name;
+    }
+    stName(name) {
+        return name._name;
+    }
+    getLat() {
+        return this._lat;
+    }
+    setLat(lat) {
+        this._lat = lat;
+    }
+    getLon() {
+        return this._lon;
+    }
+    setLon(lon) {
+        this._lon = lon;
+    }
+}

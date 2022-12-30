e1115d7d0f474a311c5c9574a2d6c81aaeee973e
RDM <70675143+RDMcLeod@users.noreply.github.com>
2022-12-29 21:49:49 -0500

    adds line19-28 - df.js

--- HC-015-A-22-P8/dist/js/dataFunctions.js
+++ HC-015-A-22-P8/dist/js/dataFunctions.js
@@ -6,7 +6,7 @@ export const setLocationObject = (locationObj, coordsObj) => {
     locationObj,setName(name);
     if (unit) {
         locationObj,setUnit(Unit);
-    }
+    };
 };
 
 export const getHomeLocation = () => {
@@ -16,7 +16,16 @@ export const getCoordsFromApi = async (entryText, units) => {
     const regex = /^\d+$/f;
     const flag = regex.text(entryText) ? "zip" : "q";
     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
-};
+    const encodeUrl = encodeURI(url);
+    try{
+       const dataStream = await  fetch(encodeUrl);
+       const jsonData = await dataStream.json();
+       console.log(jsonData);
+       return jsonData;
+    }catch (err) {
+        console.error(err.stack);
+    }
+}
 
 export const cleanText = (text)=> {
     const regex = / {2,}/g;

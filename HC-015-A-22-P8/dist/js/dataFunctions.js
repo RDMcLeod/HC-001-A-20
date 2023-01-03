6b933df33a0a89dd81193648b6934df03227d7e0
RDM <70675143+RDMcLeod@users.noreply.github.com>
2023-01-03 18:41:37 -0500

    adds/df.js weatherstrem

--- HC-015-A-22-P8/dist/js/dataFunctions.js
+++ HC-015-A-22-P8/dist/js/dataFunctions.js
@@ -18,10 +18,14 @@ export const getGeoWeatherFromCoords = async (locationObj) => {
     const lon = locationObj.getLon();
     const units = locationObj.getUnit();
     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
+    try {
+        const weatherStream = await fetch(url);
+        const weatherJson =await weatherStream.json();
+        return weatherJson;
+    } catch (err) {
+        console.log(err);
+    }
 }
-
-
-
 
 export const getCoordsFromApi = async (entryText, units) => {
     const regex = /^\d+$/f;

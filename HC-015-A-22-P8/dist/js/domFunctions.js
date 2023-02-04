266daefb92d5d0e973d8e781f7e2cbde6944ed93
RDM <70675143+RDMcLeod@users.noreply.github.com>
2023-02-03 22:00:41 -0500

    add/update

--- HC-015-A-22-P8/dist/js/domFunctions.js
+++ HC-015-A-22-P8/dist/js/domFunctions.js
@@ -40,6 +40,7 @@ const updateWeatherLocationHeader  = (message) => {
       });
       const lat = mapArray[0].indexOf("-") === -1 ? mapArray[0].slice(0, 10) : mapArray[0].slice(0, 11);
       const lon = mapArray[1].indexOf("-") === -1 ? mapArray[1].slice(0, 11) : mapArray[0].slice(0, 12);
+      h1.textContent = `${lat} • ${lon}`;
     } else {
         h1.textContent = message;
     }
